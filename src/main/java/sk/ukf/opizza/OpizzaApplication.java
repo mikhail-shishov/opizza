9c882e3f8f7b96c11bfaf544220605b47e6e95c6
Mikhail Shishov <shishovmike96@gmail.com>
2025-12-07 17:53:25 +0100

    Initial commit

---
+++ src/main/java/sk/ukf/opizza/OpizzaApplication.java
@@ -0,0 +1,13 @@
+package sk.ukf.opizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpizzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpizzaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sk/ukf/opizza/OpizzaApplicationTests.java
@@ -0,0 +1,13 @@
+package sk.ukf.opizza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpizzaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
